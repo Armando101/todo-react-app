68f77423de9380afa38af9d3ecdd716d409502e9
Armando Rivera <rivera.armando997@gmail.com>
2022-09-07 15:03:30 -0500

    Initial template for project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// import { todos } from "./mocks/todo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <TodoCounter /> */}
+      <h2>Has comletado 2 de 3 Todo's</h2>
+      {/* <TododSearch /> */}
+      <input type="text" placeholder="Buy apples" />
+      {/* <TodoList>
+        {todos.map((item, index) => {
+          <TodoItem key={index} item />;
+        })}
+      </TodoList> */}
+      {/*
+      <CreateTodoButton />
+       */}
+      <button>+</button>
+    </>
   );
 }
 
