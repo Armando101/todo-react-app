087c2c1d5b54c3144589722d3847b7256d50118b
Armando Rivera <rivera.armando997@gmail.com>
2023-01-03 16:24:10 -0600

    Migrate useState to useReducer

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,12 +1,27 @@
 import { useMemo } from "react";
+import { useReducer } from "react";
 import { useEffect } from "react";
-import { useState } from "react";
 
 export function useLocalStorage(itemName, initialValue) {
-  const [item, setItem] = useState([...initialValue]);
-  const [sincronizedItem, setSincronizedItem] = useState(true);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+  const [state, dispatch] = useReducer(reducer, initialState({ initialValue }));
+  const { item, sincronizedItem, loading, error } = state;
+
+  // ACTION CREATORS
+  const onError = (payload) => {
+    dispatch({ type: actionTypes.error, payload });
+  };
+
+  const onLoading = (payload) => {
+    dispatch({ type: actionTypes.loading, payload });
+  };
+
+  const onSincronized = (payload) => {
+    dispatch({ type: actionTypes.sincronize, payload });
+  };
+
+  const onSetItem = (payload) => {
+    dispatch({ type: actionTypes.setItem, payload });
+  };
 
   // eslint-disable-next-line
   const defaultValue = useMemo(() => initialValue, []);
@@ -22,29 +37,66 @@ export function useLocalStorage(itemName, initialValue) {
         } else {
           parsedItem = JSON.parse(localStorageItem);
         }
-        setItem(parsedItem);
-        setLoading(false);
-        setSincronizedItem(true);
+        onSetItem(parsedItem);
+        onLoading(false);
+        onSincronized(true);
       }, 1500);
     } catch {
-      setError(true);
+      onError(true);
     }
     // eslint-disable-next-line
   }, [defaultValue, sincronizedItem]);
 
   const savedItem = (newItem) => {
     try {
-      setItem(newItem);
+      onSetItem(newItem);
       localStorage.setItem(itemName, JSON.stringify(newItem));
     } catch {
-      setError(true);
+      onError(true);
     }
   };
 
   const sincronize = () => {
-    setLoading(true);
-    setSincronizedItem(false);
+    onLoading(true);
+    onSincronized(false);
   };
 
   return { item, savedItem, loading, error, sincronize };
 }
+
+const actionTypes = {
+  error: "ERROR",
+  loading: "LOADING",
+  sincronize: "SINCRONIZE",
+  setItem: "SET_ITEM",
+};
+
+const initialState = ({ initialValue }) => ({
+  item: initialValue,
+  sincronizedItem: true,
+  loading: true,
+  error: false,
+});
+
+const reducerObject = (state, payload) => ({
+  [actionTypes.error]: {
+    ...state,
+    error: payload,
+  },
+  [actionTypes.loading]: {
+    ...state,
+    loading: payload,
+  },
+  [actionTypes.sincronize]: {
+    ...state,
+    sincronizedItem: payload,
+  },
+  [actionTypes.setItem]: {
+    ...state,
+    item: payload,
+  },
+});
+
+const reducer = (state, action) => {
+  return reducerObject(state, action.payload)[action.type] || state;
+};
